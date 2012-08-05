ac6ec8cb681ed8e85d173b2fe743c15c85780041
José Mota <josemota.net@gmail.com>
2012-08-05 20:02:19 +0100

    Issue's title should be unique, with custom message.

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -2,4 +2,5 @@ class Issue < ActiveRecord::Base
   attr_accessible :description, :no_followers, :title
 
   validates_presence_of :title
+  validates_uniqueness_of :title, message: "should be unique, sir."
 end
