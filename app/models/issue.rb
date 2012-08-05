02adc07accb2a3662c9ce1d49dc553f03d0f9110
José Mota <josemota.net@gmail.com>
2012-08-05 20:05:06 +0100

    Allow the issue to not have a value for the number of followers.

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -5,5 +5,5 @@ class Issue < ActiveRecord::Base
   validates_uniqueness_of :title, message: "should be unique, sir."
 
   validates_length_of :description, minimum: 10, maximum: 12 #, is: 6
-  validates_numericality_of :no_followers
+  validates_numericality_of :no_followers, allow_blank: true
 end
