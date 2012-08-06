b62e4e4cd3193a3820620d21daf2e510ef81d7d5
José Mota <josemota.net@gmail.com>
2012-08-06 19:14:55 +0100

    Add SMTP settings for development.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,15 @@ Issues::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: ENV["MAIL_SERVER"],
+    port: 25,
+    user_name: ENV["MAIL_USERNAME"],
+    password: ENV["MAIL_PASSWORD"],
+    authentication: :plain
+  }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
